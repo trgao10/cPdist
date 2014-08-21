94d16718d4c4a096e4c32a91e02d7c266d32e7e7
Tingran <trgao10@math.duke.edu>
2014-08-21 09:17:24 -0400

    composed and tested ImprProcessRslts.m

--- ImprProcessRslts.m
+++ ImprProcessRslts.m
@@ -19,6 +19,12 @@ TextureCoords2Matrix_path = [result_path 'TextureCoords2Matrix/'];
 touch(TextureCoords1Matrix_path);
 touch(TextureCoords2Matrix_path);
 
+%%% clean up texture coordinates matrices
+command_text = ['!rm -f ' TextureCoords1Matrix_path '*'];
+eval(command_text); disp(command_text);
+command_text = ['!rm -f ' TextureCoords2Matrix_path '*'];
+eval(command_text); disp(command_text);
+
 %%% load taxa codes
 taxa_file = [data_path 'teeth_taxa_table.mat'];
 taxa_code = load(taxa_file);
@@ -31,8 +37,8 @@ ImprDistMatrix = zeros(GroupSize,GroupSize);
 ImprMapsMatrix = cell(GroupSize,GroupSize);
 invImprMapsMatrix = cell(GroupSize,GroupSize);
 lmkMSEMatrix = zeros(GroupSize,GroupSize);
-TextureCoords1Matrix = cell(GroupSize,GroupSize);
-TextureCoords2Matrix = cell(GroupSize,GroupSize);
+tmpTextureCoords1Matrix = cell(GroupSize,GroupSize);
+tmpTextureCoords2Matrix = cell(GroupSize,GroupSize);
 
 cnt = 0;
 job_id = 0;
@@ -47,29 +53,47 @@ for k1=1:GroupSize
         ImprMapsMatrix{k1,k2} = Imprrslt{k1,k2}.ImprMap;
         invImprMapsMatrix{k1,k2} = Imprrslt{k1,k2}.invImprMap;
         lmkMSEMatrix(k1,k2) = Imprrslt{k1,k2}.lkMSE;
-        TextureCoords1Matrix{k1,k2} = Imprrslt{k1,k2}.TextureCoords1;
-        TextureCoords2Matrix{k1,k2} = Imprrslt{k1,k2}.TextureCoords2;
+        tmpTextureCoords1Matrix{k1,k2} = Imprrslt{k1,k2}.TextureCoords1;
+        tmpTextureCoords2Matrix{k1,k2} = Imprrslt{k1,k2}.TextureCoords2;
         
         cnt = cnt+1;
     end
 end
 
 %%% symmetrize
+cnt = 0;
+job_id = 0;
 for j=1:GroupSize
     progressbar(j,GroupSize,20);
     for k=1:GroupSize
+        if mod(cnt,chunk_size)==0
+            if cnt>0
+                save([TextureCoords1Matrix_path 'TextureCoords1Matrix_' num2str(job_id) '.mat'],'TextureCoords1Matrix');
+                save([TextureCoords2Matrix_path 'TextureCoords2Matrix_' num2str(job_id) '.mat'],'TextureCoords2Matrix');
+                clear TextureCoords1Matrix TextureCoords2Matrix
+            end
+            job_id = job_id+1;
+            TextureCoords1Matrix = cell(GroupSize,GroupSize);
+            TextureCoords2Matrix = cell(GroupSize,GroupSize);
+        end
         if ImprDistMatrix(j,k)<ImprDistMatrix(k,j)
             lmkMSEMatrix(k,j) = lmkMSEMatrix(j,k);
             ImprMapsMatrix{k,j} = invImprMapsMatrix{j,k};
-            TextureCoords1Matrix{k,j} = TextureCoords2Matrix{j,k};
-            TextureCoords2Matrix{k,j} = TextureCoords1Matrix{j,k};
+            TextureCoords1Matrix{j,k} = tmpTextureCoords1Matrix{j,k};
+            TextureCoords2Matrix{j,k} = tmpTextureCoords2Matrix{j,k};
         else
             lmkMSEMatrix(j,k) = lmkMSEMatrix(k,j);
             ImprMapsMatrix{j,k} = invImprMapsMatrix{k,j};
-            TextureCoords1Matrix{j,k} = TextureCoords2Matrix{k,j};
-            TextureCoords2Matrix{j,k} = TextureCoords1Matrix{k,j};
+            TextureCoords1Matrix{j,k} = tmpTextureCoords2Matrix{k,j};
+            TextureCoords2Matrix{j,k} = tmpTextureCoords1Matrix{k,j};
         end
+        cnt = cnt+1;
     end
+end
+if mod(cnt,chunk_size)~=0
+    save([TextureCoords1Matrix_path 'TextureCoords1Matrix_' num2str(job_id) '.mat'],'TextureCoords1Matrix');
+    save([TextureCoords2Matrix_path 'TextureCoords2Matrix_' num2str(job_id) '.mat'],'TextureCoords2Matrix');
+    clear TextureCoords1Matrix TextureCoords2Matrix
 end
 ImprDistMatrix = min(ImprDistMatrix,ImprDistMatrix');
 
