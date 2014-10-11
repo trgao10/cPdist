79ff4d2eaef065f28262d0edb107942e12165e7d
Tingran Gao <trgao10@math.duke.edu>
2014-10-10 23:33:27 -0400

    modified for Clement's data set

---
+++ samples/check_landmarks.m
@@ -0,0 +1,10 @@
+Name = '01';
+
+A = Mesh('off','~/Work/MATLAB/DATA/Clement/meshes/01.off');
+load('~/Work/MATLAB/DATA/Clement/landmarks_clement.mat');
+landmarks = squeeze(PP(1,:,:));
+
+A.draw();
+hold on;
+scatter3(landmarks(:,1),landmarks(:,2),landmarks(:,3),30,'b','filled');
+
